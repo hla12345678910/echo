25a60fa77c4d2110b649c810490864336bc6f010
MuC <hla12345678910@gmail.com>
2014-11-01 17:30:05 -0400

    Initial Commit

---
+++ js/effects.js
@@ -0,0 +1,344 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContext();
+var audioInput = null,
+    realAudioInput = null,
+    effectInput = null,
+    wetGain = null,
+    dryGain = null,
+    outputMix = null,
+    currentEffectNode = null,
+    reverbBuffer = null,
+    dtime = null,
+    dregen = null,
+    lfo = null,
+    cspeed = null,
+    cdelay = null,
+    cdepth = null,
+    scspeed = null,
+    scldelay = null,
+    scrdelay = null,
+    scldepth = null,
+    scrdepth = null,
+    fldelay = null,
+    flspeed = null,
+    fldepth = null,
+    flfb = null,
+    sflldelay = null,
+    sflrdelay = null,
+    sflspeed = null,
+    sflldepth = null,
+    sflrdepth = null,
+    sfllfb = null,
+    sflrfb = null,
+    rmod = null,
+    mddelay = null,
+    mddepth = null,
+    mdspeed = null,
+    lplfo = null,
+    lplfodepth = null,
+    lplfofilter = null,
+    awFollower = null,
+    awDepth = null,
+    awFilter = null,
+    ngFollower = null,
+    ngGate = null,
+    bitCrusher = null,
+    btcrBits = 16,   // between 1 and 16
+    btcrNormFreq = 1; // between 0.0 and 1.0
+
+var rafID = null;
+var analyser1;
+var analyserView1;
+
+function convertToMono( input ) {
+    var splitter = audioContext.createChannelSplitter(2);
+    var merger = audioContext.createChannelMerger(2);
+
+    input.connect( splitter );
+    splitter.connect( merger, 0, 0 );
+    splitter.connect( merger, 0, 1 );
+    return merger;
+}
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame;
+window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame;
+
+function cancelAnalyserUpdates() {
+    if (rafID)
+        window.cancelAnimationFrame( rafID );
+    rafID = null;
+}
+
+function updateAnalysers(time) {
+    analyserView1.doFrequencyAnalysis( analyser1 );
+    analyserView2.doFrequencyAnalysis( analyser2 );
+    
+    rafID = window.requestAnimationFrame( updateAnalysers );
+}
+
+var lpInputFilter=null;
+
+// this is ONLY because we have massive feedback without filtering out
+// the top end in live speaker scenarios.
+function createLPInputFilter(output) {
+    lpInputFilter = audioContext.createBiquadFilter();
+    lpInputFilter.frequency.value = 2048;
+    return lpInputFilter;
+}
+
+
+function toggleMono() {
+    if (audioInput != realAudioInput) {
+        audioInput.disconnect();
+        realAudioInput.disconnect();
+        audioInput = realAudioInput;
+    } else {
+        realAudioInput.disconnect();
+        audioInput = convertToMono( realAudioInput );
+    }
+
+    createLPInputFilter(audioInput);
+    lpInputFilter.connect(dryGain);
+    lpInputFilter.connect(analyser1);
+    lpInputFilter.connect(effectInput);
+}
+
+var useFeedbackReduction = true;
+
+function gotStream(stream) {
+    // Create an AudioNode from the stream.
+//    realAudioInput = audioContext.createMediaStreamSource(stream);
+    var input = audioContext.createMediaStreamSource(stream);
+
+/*
+    realAudioInput = audioContext.createBiquadFilter();
+    realAudioInput.frequency.value = 60.0;
+    realAudioInput.type = realAudioInput.NOTCH;
+    realAudioInput.Q = 10.0;
+
+    input.connect( realAudioInput );
+*/
+    audioInput = convertToMono( input );
+
+    if (useFeedbackReduction) {
+        audioInput.connect( createLPInputFilter() );
+        audioInput = lpInputFilter;
+        
+    }
+    // create mix gain nodes
+    outputMix = audioContext.createGain();
+    dryGain = audioContext.createGain();
+    wetGain = audioContext.createGain();
+    effectInput = audioContext.createGain();
+    audioInput.connect(dryGain);
+    audioInput.connect(analyser1);
+    audioInput.connect(effectInput);
+    dryGain.connect(outputMix);
+    wetGain.connect(outputMix);
+    outputMix.connect( audioContext.destination);
+    outputMix.connect(analyser2);
+    crossfade(1.0);
+    changeEffect();
+    cancelAnalyserUpdates();
+    updateAnalysers();
+}
+
+function changeInput(){
+  if (!!window.stream) {
+    window.stream.stop();
+  }
+  var audioSelect = document.getElementById("audioinput");
+  var audioSource = audioSelect.value;
+  var constraints = 
+  {
+    "audio": {
+        "mandatory": {
+            "googEchoCancellation": "false",
+            "googAutoGainControl": "false",
+            "googNoiseSuppression": "false",
+            "googHighpassFilter": "false"
+        },
+        "optional": [{sourceId: audioSource}]
+    }
+  };
+  navigator.getUserMedia(constraints, gotStream, function(e) {
+            alert('Error getting audio');
+            console.log(e);
+        });
+}
+
+function gotSources(sourceInfos) {
+    var audioSelect = document.getElementById("audioinput");
+    while (audioSelect.firstChild)
+        audioSelect.removeChild(audioSelect.firstChild);
+
+    for (var i = 0; i != sourceInfos.length; ++i) {
+        var sourceInfo = sourceInfos[i];
+        if (sourceInfo.kind === 'audio') {
+            var option = document.createElement("option");
+            option.value = sourceInfo.id;
+            option.text = sourceInfo.label || 'input ' + (audioSelect.length + 1);
+            audioSelect.appendChild(option);
+        }
+    }
+    audioSelect.onchange = changeInput;
+}
+
+function initAudio() {
+    var irRRequest = new XMLHttpRequest();
+    irRRequest.open("GET", "sounds/cardiod-rear-levelled.wav", true);
+    irRRequest.responseType = "arraybuffer";
+    irRRequest.onload = function() {
+        audioContext.decodeAudioData( irRRequest.response, 
+            function(buffer) { reverbBuffer = buffer; } );
+    }
+    irRRequest.send();
+
+    o3djs.require('o3djs.shader');
+
+    analyser1 = audioContext.createAnalyser();
+    analyser1.fftSize = 1024;
+    analyser2 = audioContext.createAnalyser();
+    analyser2.fftSize = 1024;
+
+    analyserView1 = new AnalyserView("view1");
+    analyserView1.initByteBuffer( analyser1 );
+    analyserView2 = new AnalyserView("view2");
+    analyserView2.initByteBuffer( analyser2 );
+
+    if (!navigator.getUserMedia)
+        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    if (!navigator.getUserMedia)
+        return(alert("Error: getUserMedia not supported!"));
+
+    navigator.getUserMedia({audio:true}, gotStream, function(e) {
+            alert('Error getting audio');
+            console.log(e);
+        });
+
+    if ((typeof MediaStreamTrack === 'undefined')||(!MediaStreamTrack.getSources)){
+        console.log("This browser does not support MediaStreamTrack, so doesn't support selecting sources.\n\nTry Chrome Canary.");
+    } else {
+        MediaStreamTrack.getSources(gotSources);
+    }
+
+    document.getElementById("effect").onchange=changeEffect;
+}
+
+function keyPress(ev) {
+    var oldEffect = document.getElementById("effect").selectedIndex;
+    var newEffect = oldEffect;
+    switch (ev.keyCode) {
+      case 50: // 'r'
+        newEffect = 1;
+        break;
+      case 49: // 'c'
+        newEffect = 8;
+        break;
+      case 51: // 'p'
+        newEffect = 10;
+        break;
+      default:
+        console.log(ev.keyCode);
+    }
+    if (newEffect != oldEffect) {
+        document.getElementById("effect").selectedIndex = newEffect;
+        changeEffect();
+    }
+}
+
+window.addEventListener('load', initAudio );
+
+window.addEventListener('keydown', keyPress );
+
+function crossfade(value) {
+  // equal-power crossfade
+  var gain1 = Math.cos(value * 0.5*Math.PI);
+  var gain2 = Math.cos((1.0-value) * 0.5*Math.PI);
+
+  dryGain.gain.value = gain1;
+  wetGain.gain.value = gain2;
+}
+
+var lastEffect = -1;
+
+function changeEffect() {
+    lfo = null;
+    dtime = null;
+    dregen = null;
+    cspeed = null;
+    cdelay = null;
+    cdepth = null;
+    rmod = null;
+    fldelay = null;
+    flspeed = null;
+    fldepth = null;
+    flfb = null;
+    scspeed = null;
+    scldelay = null;
+    scrdelay = null;
+    scldepth = null;
+    scrdepth = null;
+    sflldelay = null;
+    sflrdelay = null;
+    sflspeed = null;
+    sflldepth = null;
+    sflrdepth = null;
+    sfllfb = null;
+    sflrfb = null;
+    rmod = null;
+    mddelay = null;
+    mddepth = null;
+    mdspeed = null;
+    lplfo = null;
+    lplfodepth = null;
+    lplfofilter = null;
+    awFollower = null;
+    awDepth = null;
+    awFilter = null;
+    ngFollower = null;
+    ngGate = null;
+    bitCrusher = null;
+
+    if (currentEffectNode) 
+        currentEffectNode.disconnect();
+    if (effectInput)
+        effectInput.disconnect();
+
+    var effect = document.getElementById("effect").selectedIndex;
+    var effectControls = document.getElementById("controls");
+    if (lastEffect > -1)
+        effectControls.children[lastEffect].classList.remove("display");
+    lastEffect = effect;
+    effectControls.children[effect].classList.add("display");
+
+    switch (effect) {
+        case 0: // Delay
+            currentEffectNode = createDelay();
+            break;
+        default:
+            break;
+    }
+    audioInput.connect( currentEffectNode );
+}
+
+function createDelay() {
+    var delayNode = null;
+    if (window.location.search.substring(1) == "webkit")
+        delayNode = audioContext.createDelay();
+    else
+        delayNode = audioContext.createDelay();
+    delayNode.delayTime.value = parseFloat( document.getElementById("dtime").value );
+    dtime = delayNode;
+
+    var gainNode = audioContext.createGain();
+    gainNode.gain.value = parseFloat( document.getElementById("dregen").value );
+    dregen = gainNode;
+
+    gainNode.connect( delayNode );
+    delayNode.connect( gainNode );
+    delayNode.connect( wetGain );
+
+    return delayNode;
+}
